9b2d1476faa0ca370f6303aa04b1aed6fe8819bf
AviChakravarti14 <avichakravarti3@gmail.com>
2023-07-11 12:13:24 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppneww
+{
+    public class Emp
+    {
+        public string Fullname(string fname, string lname)
+        {
+            return fname+ "  " +lname;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        { Emp emp = new Emp();
+            Console.WriteLine(emp.Fullname("Avi", "Chakravarti"));
+            Console.ReadKey();
+        }
+    }
+}
